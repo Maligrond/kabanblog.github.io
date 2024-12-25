acfac2a29113e42955cac661933107c83856ec52
Maligrond <stormyweatherlight@gmail.com>
2024-12-25 17:34:29 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://kabanblog.github.io',
+	base: 'kabanblog.github.io',
 	integrations: [mdx(), sitemap()],
 });
